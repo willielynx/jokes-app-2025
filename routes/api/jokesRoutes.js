b5981287bda55c1e0e8abce90ba253f133993df3
willielynx <wptolar@gmail.com>
2025-10-27 12:45:55 -0500

    2nd commit

--- routes/api/jokesRoutes.js
+++ routes/api/jokesRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const axios = require('axios')
 const PORT = process.env.PORT || 3001
 
 // http://localhost:PORT/jokes
 router.get('/', (req,res) => {
-    res.send('This works')
+    // res.send('This works')
+    const url = 'https://api.sampleapis.com/jokes/goodJokes'
+    axios.get(url).then(resp => console.log(resp)).then(res.send('data received'))
 })
 
 
